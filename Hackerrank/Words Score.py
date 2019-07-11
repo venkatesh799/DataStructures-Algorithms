9fd51ac1f46c9b7072505f2132bf23b639b851ba
venkatesh thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2019-07-11 10:53:33 +0530

    Update Words Score.py

--- Hackerrank/Words Score.py
+++ Hackerrank/Words Score.py
@@ -3,7 +3,7 @@ Consider that vowels in the alphabet are a, e, i, o, u and y.
 
 Function score_words takes a list of lowercase words as an argument and returns a score as follows:
 
-The score of a single word is  if the word contains an even number of vowels. Otherwise, the score of this word is . The score for the whole list of words is the sum of scores of all words in the list.
+The score of a single word is 1 if the word contains an even number of vowels. Otherwise, the score of this word is 2 . The score for the whole list of words is the sum of scores of all words in the list.
 
 Debug the given function score_words such that it returns a correct score.
 
