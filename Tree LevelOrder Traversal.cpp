390408847ab5ba825b03ce4328d4c795ab12cf3e
venkatesh thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2020-04-27 13:15:15 +0530

    Update Tree LevelOrder Traversal.cpp

--- Tree LevelOrder Traversal.cpp
+++ Tree LevelOrder Traversal.cpp
@@ -1,3 +1,5 @@
+//Tree LevelOrder in Singleline
+
 void levelorder(struct node *p)
 {   
     
@@ -12,6 +14,34 @@ void levelorder(struct node *p)
         if(current->left != NULL) Q.push(current->left);
         if(current->right != NULL) Q.push(current->right);
         Q.pop();
+    }
+
+}
+
+
+
+//Tree LevelOrder Traversal  line By Line
+
+void levelorder(struct node *p)
+{   
+
+    if(p == NULL)
+        return;
+    queue<struct node*> q;
+    q.push(p);
+    while(!q.empty())
+    {
+        int count=q.size();
+        while(count > 0)
+        {
+            struct node *current=q.front();
+            cout<<current->data<<" ";
+            if(current->left != NULL) q.push(current->left);
+            if(current->right != NULL) q.push(current->right); 
+             q.pop();
+            count-=1;
+        }
+        cout<<"\n";
     }
 
 }
