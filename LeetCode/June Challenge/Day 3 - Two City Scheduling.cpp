28f69b5bdf97209d59c3edc551dac6527a416070
venkatesh thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2020-06-06 12:23:17 +0530

    Create Day 3 - Two City Scheduling.cpp

---
+++ LeetCode/June Challenge/Day 3 - Two City Scheduling.cpp
@@ -0,0 +1,29 @@
+/*
+
+https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3349/
+
+*/
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) 
+    {
+        int n=costs.size();
+        sort(costs.begin(),costs.end(),[](vector<int> &a,vector<int> &b)
+        {
+          if(a[1] - a[0] > b[1] - b[0])
+              return true;
+          return false;
+        });
+        
+        int total=0;
+        for(int i=0;i<n/2;i++)
+        {
+            total+=costs[i][0];
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            total+=costs[i][1];
+        }
+        return total;
+    }
+};
