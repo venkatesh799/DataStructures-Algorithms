343451f9a46761dfabac2c47193fc5ea202843d6
Venkatesh Thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2020-06-11 11:38:52 +0530

    Create Is Subsequence.cpp

---
+++ LeetCode/June Challenge/Is Subsequence.cpp
@@ -0,0 +1,49 @@
+/*
+
+Given a string s and a string t, check if s is subsequence of t.
+
+A subsequence of a string is a new string which is formed from the original string by deleting
+some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+(ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, 
+and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Input: s = "axc", t = "ahbgdc"
+Output: false     */
+
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t)
+    {
+    int i,j,index=INT_MIN;
+    char temp;
+    bool ans=false;
+    for(i=0;i<s.size();i++)
+    {
+        temp=s[i];
+        ans=false;
+        for(j=i;j<t.size();j++)
+        {
+            if(temp == t[j] && j > index )
+            {
+                ans=true;
+                index=j;
+                break;
+            }
+        }
+        if(!ans)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+    }
+};
+
