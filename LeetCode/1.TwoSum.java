112ca6aa91f7c0e1a43a31d8970d091dcafe53d9
Venkatesh Thirunagiri <tvenkatesh799@gmail.com>
2024-12-13 11:59:04 -0600

    Create 1.TwoSum.java

---
+++ LeetCode/1.TwoSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            int diff = target-nums[i];
+            if(mp.containsKey(diff)) {
+                return new int[]{mp.get(diff), i};
+            }
+            mp.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
